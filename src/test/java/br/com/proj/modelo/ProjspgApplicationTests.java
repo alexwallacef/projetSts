e160843b7ffb8040a2384d6a73fb16a0d81515d5
Alex Wallace <alexwallace.garcia@gmail.com>
2018-11-17 14:19:37 -0200

    Rest Testado

---
+++ src/test/java/br/com/proj/modelo/ProjspgApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.proj.modelo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProjspgApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
